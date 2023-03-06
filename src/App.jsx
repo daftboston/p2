55a573cec2650ae7c067e483fac4980a4b391825
daftboston <dsantoyop@gmail.com>
2023-03-06 16:10:28 -0500

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from "axios"
 import Weather from './components/Weather'
 
 import './App.css'
@@ -10,16 +11,40 @@ function App() {
 
   useEffect (()=>{
   axios
-  .get ("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+  .get (`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`)
   .then(resp => console.log(resp.data))
   .catch ( error => console.error(error) )
   },[])
 
- 
-
-  const [index, setindex]=useState(0)
+  
 
 
+  const [lat, setLat]=useState( 4.6775397)
+  const [long, setLong] = useState (-74.1122391)
+
+
+
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  
+  function success(pos) {
+    const crd = pos.coords;
+  
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
 
 
   return (
