3ea7251a00cac87d8121d4deec806af3eefa7bc2
daftboston <dsantoyop@gmail.com>
2023-03-06 14:22:18 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Component, useState } from 'react'
+import { useEffect, useState } from 'react'
+import Weather from './components/Weather'
 
 import './App.css'
 
@@ -7,14 +8,26 @@ import './App.css'
 
 function App() {
 
+  useEffect (()=>{
+  axios
+  .get ("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+  .then(resp => console.log(resp.data))
+  .catch ( error => console.error(error) )
+  },[])
+
  
 
   const [index, setindex]=useState(0)
 
 
 
+
   return (
     <div className="App">
+
+      <Weather></Weather>
+
+
         
     
     </div>
---
+++ src/components/Weather.jsx
@@ -0,0 +1,8 @@
+const Weather = () =>  {
+    return (
+<div>
+    <h1>app clima</h1>
+</div>
+)
+}
+export default Weather
