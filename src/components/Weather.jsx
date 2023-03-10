1482231ac6fd82d41422a6345c5da9a4edba4600
daftboston <dsantoyop@gmail.com>
2023-03-10 16:52:19 -0500

    sa

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,10 @@
 const Weather = ({data}) =>  {
     return (
-<div className="target">
+<div>
     <h1>app clima</h1>
     <h1>pais : {data.base}</h1>
     <div>
-       <img  src="/m3.jpg" alt="icons"/>
+       <img src="/01d.png" alt="icons"/>
     </div>
 </div>
 )
