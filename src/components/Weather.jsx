faa1e0221baa912ec2f8d613d4d4a187a6c6061e
daftboston <dsantoyop@gmail.com>
2023-03-28 15:14:55 -0500

    save

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,6 +4,8 @@ const Weather = ({data}) =>  {
     <h1>app clima</h1>
     <h1>pais : {data.base}</h1>
     <img src="/icons/01d.png" alt="icons"/>
+    <h3>temperature: {data.main?.temp}</h3>
+    <h4>descripcion:{data.weather[0]} </h4>
 </div>
 )
 }
