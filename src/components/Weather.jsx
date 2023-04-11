3fa18014d96b0d1f89ceda762bce936bf4130b8d
Daniel <dsantoyop@gmail.com>
2023-04-10 22:21:38 -0500

    save

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,12 @@
 const Weather = ({data}) =>  {
     return (
-<div>
-    <h1>app clima</h1>
-    <h1>pais : {data.base}</h1>
+<div className="card">         
+    <h1>pais : {data.sys?.country}</h1>
+    <h3>ubicacion: {data.name}</h3>
     <img src="/icons/01d.png" alt="icons"/>
-    <h3>temperature: {data.main?.temp}</h3>
-    <h4>descripcion:{data.weather[0]} </h4>
+    <h3>temperature: {data.main?.temp} °</h3>
+    <h4>descripcion: {data?.weather?.[0].main}</h4>
+    <h4>{data.weather?.[0].description}</h4>
 </div>
 )
 }
