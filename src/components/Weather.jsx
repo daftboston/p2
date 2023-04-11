c7e0c14c3549900fd4e8586a18d20adb15c6ecf7
daftboston <dsantoyop@gmail.com>
2023-04-11 17:03:54 -0500

    save

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -16,7 +16,7 @@ const Weather = ({data,grades}) =>  {
     <h1>pais : {data.sys?.country}</h1>
     <h3>ubicacion: {data.name}</h3>
     <img src="/icons/01d.png" alt="icons"/>
-    <h3>temperature: {isFar?[grades]:[(grades*9/5)+32]} °</h3>
+    <h3>temperature: {isFar?[grades]:[(grades*9/5)+32]}{ isFar ? " C" : " F" } °</h3>
     <h4>descripcion: {data?.weather?.[0].main}</h4>
     <h4>{data.weather?.[0].description}</h4>
     <button onClick={()=>setIsFar(!isFar)}>convert</button>
